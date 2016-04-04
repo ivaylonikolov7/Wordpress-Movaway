f473e0969493c47cb6bf8cc4bd217d403ed20148
IvayloNikolov <bloodydoom3@gmail.com>
2016-04-04 18:55:16 +0300

    wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'abcsigma_wordpresss');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'abcsigma_admin');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'xaxaxa123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
